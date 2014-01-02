3e02392b80d1cf9dd9684292ce79bbba2cb69fd0
Timothy Lee <timmy.ccl@gmail.com>
2014-01-02 17:32:54 +0800

    version 0.0.2

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :issues_with_me do
   name 'Issues With Me plugin'
   author 'Timothy Lee'
   description 'Lists issues have been assigned to me'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/ctiml/redmine_issues_with_me'
   author_url ''
 end
